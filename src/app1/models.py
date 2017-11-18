8192c22e88c77751b07230119e562eb96b53f2e8
abelko <esung1129@gmail.com>
2017-11-19 02:07:44 +0900

    setting models
    no setting static files

--- src/app1/models.py
+++ src/app1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Resume(models.Model):
+    name = models.CharField(max_length=15)
+    github_link = models.CharField(max_length=30)
+    view_count = models.IntegerField()
+
+class Stack(models.Model):
+    resume = models.ForeignKey(Resume)
+    development_language = models.CharField(max_length=30)
