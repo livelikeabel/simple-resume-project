9ca829d7b7f5f5c0d9c158adc42d928758c1be62
abelko <esung1129@gmail.com>
2017-11-19 02:50:47 +0900

    Add templates

--- src/app1/views.py
+++ src/app1/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+
+#def index(request):
+#    return HttpResponse(" Hello, Abel.  It's polls index. ;) ")
 
 def index(request):
-    return HttpResponse(" Hello, Abel.  It's polls index. ;) ")
+    return render(request, "index.html", {})
